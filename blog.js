adbc3fb9296ed58c934fa30148fc72e3e2cbe6ee
SeoYounSeok <syounseok94@gmail.com>
2022-08-04 11:50:02 +0900

    주석 정리

--- blog.js
+++ blog.js
@@ -37,19 +37,6 @@ import auth from "./assets/auth.js";
   await blogPage.waitForSelector(".img_common_tistory.link_edit");
   await blogPage.click(".img_common_tistory.link_edit");
 
-  // inputArea = await page.$(".gLFyf");
-  // searchBtn = await page.$(
-  //   ".FPdoLc > center:nth-child(1) > input:nth-child(1)"
-  // );
-
-  // await inputArea.type("puppeteer test");
-  // await searchBtn.click();
-
-  // await page.waitForNavigation();
-
   // `screen1.png` 스크린샷을 캡처 하여 screen 폴더에 저장
   // await page.screenshot({ path: "./screen/screen1.png" });
-
-  // 모든 스크래핑 작업을 마치고 브라우저 닫기
-  //await browser.close();
 })();
